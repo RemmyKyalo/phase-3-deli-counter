4d63cc8f93b5a96334205e11c7b7037da3b73e72
remmy <chaloremmygmail.comgit config --global user.name remmygit config --global user.email chaloremmygmail.com>
2023-02-10 21:14:41 +0300

    Completed Assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+def line(katz_deli) 
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    line_list = katz_deli.map.with_index do |person, index|
+      "#{index + 1}. #{person}"
+    end
+    puts "The line is currently: #{line_list.join(" ")}"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    name = katz_deli.shift
+    puts "Currently serving #{name}."
+  end
+end
+
+
